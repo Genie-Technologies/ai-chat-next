d47784960b091ea2b01c353b8781f476ce3d8bb9
Shagun Mistry <shagun.mistry@hotmail.com>
2023-02-05 18:29:35 -0500

    Update

---
+++ components/Footer.tsx
@@ -0,0 +1,13 @@
+import AppBar from "@mui/material/AppBar";
+
+export const Footer = () => {
+  return (
+    <AppBar
+      position="fixed"
+      color="primary"
+      sx={{ top: "auto", bottom: 0 }}
+    ></AppBar>
+  );
+};
+
+export default Footer;
