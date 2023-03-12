2c652cf9d3b1101781906574b30e26b4a48896f1
Shagun Mistry <shagun.mistry@hotmail.com>
2023-03-12 18:44:13 -0400

    Update favicon

--- pages/captionr.tsx
+++ pages/captionr.tsx
@@ -4,44 +4,18 @@ import Paper from "@mui/material/Paper";
 import { Main } from "../components/layouts";
 import Head from "next/head";
 import CaptionrCard from "../components/Captionr/CaptionrCard";
+import { croppedLogoSrc } from "../components/utils";
 
 export default function ChatPage() {
   return (
     <Main>
       <Head>
-        <title>CaptionR - ResponAi</title>
+        <title>CaptionR By ResponAi</title>
+        <meta name="description" content="CaptionR By ResponAi" />
+        <link rel="icon" href={croppedLogoSrc} />
       </Head>
       <Paper>
         <CaptionrCard />
-
-        {/* <FormControl>
-          <Typography variant="h6" component="h6">
-            URL
-          </Typography>
-          <TextField id="outlined-basic" label="URL" variant="outlined" />
-
-          <Button
-            variant="contained"
-            onClick={() => {
-              const url = document.getElementById(
-                "outlined-basic"
-              ) as HTMLInputElement;
-              const urlValue = url.value;
-
-              fetch("/api/captionr", {
-                method: "POST",
-                headers: {
-                  "Content-Type": "application/json",
-                },
-                body: JSON.stringify({ imageFile: urlValue }),
-              }).then((response) => {
-                console.log(response);
-              });
-            }}
-          >
-            Submit
-          </Button>
-        </FormControl> */}
       </Paper>
     </Main>
   );
